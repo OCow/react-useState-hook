96b426c2c34ab89b9be47ef784b5be5ec93a9076
oliver.kuhn2@gmail.com <oliver.kuhn2@gmail.com>
2020-11-01 20:59:41 +0100

    implement counter

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
-  return <div />;
+  //const state = React.useState();
+  let [counterValue, setCountFunction] = useState(0);
+
+  console.log(counterValue);
+
+  function increase() {
+    setCountFunction(counterValue + 1);
+  }
+
+  function decrease() {
+    setCountFunction(counterValue - 1);
+  }
+
+  return (
+    <div className="container">
+      <h1>{counterValue}</h1>
+      <button onClick={increase}>+</button>
+      <button onClick={decrease}>-</button>
+    </div>
+  );
 }
 
 export default App;
